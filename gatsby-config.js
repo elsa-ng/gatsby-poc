cac3eecc630703431206ceae0352736cf163c76f
Elsa Ng <elsa.ng@ontario.ca>
2022-03-29 12:45:59 -0400

    Added sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: `Little Round Head`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        sassOptions: {
+          includePaths: ["./node_modules"],
+        }
+      },
+    }
+  ],
 };
